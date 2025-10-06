08dcb55565a49b5b1dff290da75006a8c108a2f3
Touscix <mat.pfeffer@yahoo.fr>
2025-10-06 23:40:31 +0300

    fix(events): correct lazy initialization and import in provider

--- app/src/main/java/com/android/joinme/model/event/EventsRepositoryProvider.kt
+++ app/src/main/java/com/android/joinme/model/event/EventsRepositoryProvider.kt
@@ -1,21 +1,19 @@
 package com.android.joinme.model.event
 
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-
 /**
  * Provides a single instance of the repository in the app. 'repository' is mutable for testing
  * purposes.
  */
 object EventsRepositoryProvider {
 
-  private val firestoreRepo: EventsRepository by lazy {
-    EventsRepositoryFirestore(db = Firebase.firestore)
-  }
+  //  private val firestoreRepo: EventsRepository by lazy {
+  //    EventsRepositoryFirestore(db = Firebase.firestore)
+  //  }
 
   private val localRepo: EventsRepository by lazy { EventsRepositoryLocal() }
 
   fun getRepository(isOnline: Boolean): EventsRepository {
-    return if (isOnline) firestoreRepo else localRepo
+    // return if (isOnline) firestoreRepo else localRepo
+    return localRepo
   }
 }
