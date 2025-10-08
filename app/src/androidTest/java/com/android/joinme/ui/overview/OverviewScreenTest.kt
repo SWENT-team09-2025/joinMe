e5d96fe1ecab889d3da58683f4e75a5785e7533a
Saifur <saifur.1870@gmail.com>
2025-10-08 10:29:01 +0200

    fix(overview): update tests in OverviewViewModelTest

--- app/src/androidTest/java/com/android/joinme/ui/overview/OverviewScreenTest.kt
+++ app/src/androidTest/java/com/android/joinme/ui/overview/OverviewScreenTest.kt
@@ -1,5 +1,6 @@
 package com.android.joinme.ui.overview
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.android.joinme.model.event.*
@@ -9,6 +10,7 @@ import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.Test
 
+@OptIn(ExperimentalMaterial3Api::class)
 class OverviewScreenTest {
 
   @get:Rule val composeTestRule = createComposeRule()
@@ -35,6 +37,10 @@ class OverviewScreenTest {
 
     composeTestRule.setContent { OverviewScreen(overviewViewModel = viewModel) }
 
+    // Wait for Compose to settle (LaunchedEffect + Flow collection)
+    composeTestRule.waitForIdle()
+    composeTestRule.mainClock.advanceTimeByFrame()
+
     composeTestRule.onNodeWithTag(OverviewScreenTestTags.EMPTY_EVENT_LIST_MSG).assertExists()
   }
 
@@ -44,22 +50,20 @@ class OverviewScreenTest {
       val repo = EventsRepositoryLocal()
       val viewModel = OverviewViewModel(repo)
 
-      // Add some events into the local repository
       repo.addEvent(createEvent("1", "Basketball", EventType.SPORTS))
       repo.addEvent(createEvent("2", "Bar", EventType.SOCIAL))
 
       composeTestRule.setContent { OverviewScreen(overviewViewModel = viewModel) }
 
       composeTestRule.waitForIdle()
+      composeTestRule.mainClock.advanceTimeByFrame()
 
       composeTestRule.onNodeWithTag(OverviewScreenTestTags.EVENT_LIST).assertExists()
-
       composeTestRule
           .onNodeWithTag(
               OverviewScreenTestTags.getTestTagForEventItem(
                   createEvent("1", "Basketball", EventType.SPORTS)))
           .assertExists()
-
       composeTestRule
           .onNodeWithTag(
               OverviewScreenTestTags.getTestTagForEventItem(
@@ -78,8 +82,8 @@ class OverviewScreenTest {
       OverviewScreen(overviewViewModel = viewModel, onAddEvent = { clicked = true })
     }
 
+    composeTestRule.waitForIdle()
     composeTestRule.onNodeWithTag(OverviewScreenTestTags.CREATE_EVENT_BUTTON).performClick()
-
     assert(clicked)
   }
 
@@ -87,7 +91,6 @@ class OverviewScreenTest {
   fun clickingEvent_triggersOnSelectEvent() = runBlocking {
     val repo = EventsRepositoryLocal()
     val viewModel = OverviewViewModel(repo)
-
     val event = createEvent("1", "Basketball", EventType.SPORTS)
     repo.addEvent(event)
 
@@ -96,6 +99,9 @@ class OverviewScreenTest {
     composeTestRule.setContent {
       OverviewScreen(overviewViewModel = viewModel, onSelectEvent = { selected = it })
     }
+
+    composeTestRule.waitForIdle()
+    composeTestRule.mainClock.advanceTimeByFrame()
 
     composeTestRule
         .onNodeWithTag(OverviewScreenTestTags.getTestTagForEventItem(event))
