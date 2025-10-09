b61618707a79c8f42910a941aa996f72f5d3b3b1
vlonfat <165289507+vlonfat@users.noreply.github.com>
2025-10-09 16:02:22 +0200

    feat(Auth): implementation of backend of SignIn
    Implementation of backend of signin with test

---
+++ app/src/androidTest/java/com/android/joinme/ui/authentification/SignInViewModelTest.kt
@@ -0,0 +1,51 @@
+package com.android.joinme.ui.authentification
+
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import com.android.joinme.ui.signIn.SignInViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.Dispatchers
+import org.junit.Before
+import org.junit.After
+import org.junit.Test
+import junit.framework.TestCase.assertFalse
+import org.junit.Assert.assertNull
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class SignInViewModelTest {
+
+    private lateinit var viewModel: SignInViewModel
+    private val testDispatcher = StandardTestDispatcher()
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
+        viewModel = SignInViewModel()
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun initialState_isCorrect() = runTest {
+        val state = viewModel.uiState.value
+        assertFalse(state.isLoading)
+        assertNull(state.user)
+        assertNull(state.errorMsg)
+        assertFalse(state.signedOut)
+    }
+
+    /*@Test
+    fun clearErrorMsg_resetsError() = runTest {
+        viewModel.signIn(context = mockContext, credentialManager = mockCredentialManager) // simulate error
+        viewModel.clearErrorMsg()
+        assertThat(viewModel.uiState.value.errorMsg).isNull()
+    }*/
+
+    // TODO: You need to mock CredentialManager and AuthRepository to test signIn properly
+}
--- app/src/main/java/com/android/joinme/MainActivity.kt
+++ app/src/main/java/com/android/joinme/MainActivity.kt
@@ -15,28 +15,29 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import com.android.joinme.model.authentification.AuthRepository
-import com.android.joinme.navigation.NavigationActions
-import com.android.joinme.navigation.Screen
-import com.android.joinme.signIn.SignInScreen
+import com.android.joinme.ui.navigation.NavigationActions
+import com.android.joinme.ui.navigation.Screen
+import com.android.joinme.ui.overview.OverviewScreen
+import com.android.joinme.ui.overview.OverviewViewModel
+import com.android.joinme.ui.signIn.SignInScreen
 import com.android.joinme.ui.theme.SampleAppTheme
 import com.google.firebase.auth.FirebaseAuth
 import okhttp3.OkHttpClient
 
-
 object HttpClientProvider {
-    var client: OkHttpClient = OkHttpClient()
+  var client: OkHttpClient = OkHttpClient()
 }
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var auth: FirebaseAuth
-    private lateinit var authRepository: AuthRepository
+  private lateinit var auth: FirebaseAuth
+  private lateinit var authRepository: AuthRepository
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-        setContent { SampleAppTheme { Surface(modifier = Modifier.fillMaxSize()) { JoinMe() } } }
-    }
+    setContent { SampleAppTheme { Surface(modifier = Modifier.fillMaxSize()) { JoinMe() } } }
+  }
 }
 
 @Composable
@@ -44,21 +45,26 @@ fun JoinMe(
     context: Context = LocalContext.current,
     credentialManager: CredentialManager = CredentialManager.create(context),
 ) {
-    val navController = rememberNavController()
-    val navigationActions = NavigationActions(navController)
-    val startDestination = Screen.Auth.name
-        //if (FirebaseAuth.getInstance().currentUser == null)
+  val navController = rememberNavController()
+  val navigationActions = NavigationActions(navController)
+  val startDestination =
+      if (FirebaseAuth.getInstance().currentUser != null) Screen.Overview.route
+      else Screen.Auth.route
 
-    NavHost(navController = navController, startDestination = startDestination) {
-        navigation(
-            startDestination = Screen.Auth.route,
-            route = Screen.Auth.name,
-        ) {
-            composable(Screen.Auth.route) {
-                SignInScreen(
-                    credentialManager = credentialManager,
-                    onSignedIn = { navigationActions.navigateTo(Screen.SecondActivity) })
-            }
-        }
+  NavHost(navController = navController, startDestination = startDestination) {
+      composable(Screen.Auth.route) {
+        SignInScreen(
+            credentialManager = credentialManager,
+            onSignedIn = { navigationActions.navigateTo(Screen.Overview) })
+      }
+
+    composable(Screen.Overview.route) {
+      OverviewScreen(
+          overviewViewModel = OverviewViewModel(),
+          credentialManager = credentialManager,
+          navigationActions = navigationActions,
+          onAddEvent = { /* to complete */},
+          onSelectEvent = { /* to complete */})
     }
+  }
 }
