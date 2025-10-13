1d5f4c67f08b758d980ccf986f6523fecddfbe3e
BryanDMP <bryan.dematos@epfl.ch>
2025-10-14 00:58:27 +0200

    style(format): re-apply formatting after rebase

--- app/src/main/java/com/android/joinme/ui/theme/Color.kt
+++ app/src/main/java/com/android/joinme/ui/theme/Color.kt
@@ -10,7 +10,5 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-
 val JoinMeColor = Color(0xFF101D26)
 val CreateEventButtonColor = Color(0xFFEDE7F6)
-
