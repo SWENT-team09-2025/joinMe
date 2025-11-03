e65d6633b1a49e68f37769ffca5a4257dfdef731
vlonfat <165289507+vlonfat@users.noreply.github.com>
2025-11-03 21:59:16 +0100

    fix(sonar): add sonarqube configuration block at root level

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,17 @@ plugins {
     alias(libs.plugins.gms) apply false
     alias(libs.plugins.sonar) apply true
 }
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "SWENT-team09-2025_joinMe")
+        property("sonar.organization", "swent-team09-2025")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.sources", "app/src/main/java")
+        property("sonar.tests", "app/src/test/java,app/src/androidTest/java")
+        property("sonar.java.binaries", "app/build/tmp/kotlin-classes/debug")
+        property("sonar.java.test.binaries", "app/build/tmp/kotlin-classes/debugUnitTest,app/build/tmp/kotlin-classes/debugAndroidTest")
+        property("sonar.coverage.jacoco.xmlReportPaths", "app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
+        property("sonar.exclusions", "**/*.png,**/*.jpg,**/*.jpeg,**/*.webp,**/*.gif,**/*.svg")
+    }
+}
