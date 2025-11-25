98b2ddc392aeef92635be0687c865d638d430ba1
AlexisPDS <alexis.poudens@epfl.ch>
2025-11-25 21:57:02 +0100

    test: add new repository method to mock function

--- app/src/test/java/com/android/joinme/ui/overview/CreateSerieViewModelTest.kt
+++ app/src/test/java/com/android/joinme/ui/overview/CreateSerieViewModelTest.kt
@@ -93,6 +93,12 @@ class CreateSerieViewModelTest {
     override suspend fun leaveGroup(groupId: String, userId: String) {}
 
     override suspend fun joinGroup(groupId: String, userId: String) {}
+      override suspend fun getCommonGroups(userIds: List<String>): List<Group> {
+          if (userIds.isEmpty()) return emptyList()
+          return groups.filter { group ->
+              userIds.all { userId -> group.memberIds.contains(userId) }
+          }
+      }
   }
 
   private lateinit var repo: FakeSeriesRepository
@@ -897,6 +903,7 @@ class CreateSerieViewModelTest {
           override suspend fun leaveGroup(groupId: String, userId: String) {}
 
           override suspend fun joinGroup(groupId: String, userId: String) {}
+            override suspend fun getCommonGroups(userIds: List<String>): List<Group> {return emptyList()}
         }
 
     val errorVm = CreateSerieViewModel(repo, errorGroupRepo)
